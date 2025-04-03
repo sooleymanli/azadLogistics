62f99ff180029b274e6a07f03039066d522618f8
Fuad Suleymanli <fuadsuleymanli@Fuad-MacBook-Pro.local>
2025-04-03 05:33:04 +0400

    Up

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
   if (token) {
